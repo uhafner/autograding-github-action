b3f8782864899e486a6bd5f622a246bba91f6807
Tobias Effner <me@tobiasmichael.de>
2020-05-06 13:11:18 +0200

    Added to workflow file

--- src/test/java/de/tobiasmichael/me/MyTest.java
+++ src/test/java/de/tobiasmichael/me/MyTest.java
@@ -6,10 +6,30 @@ import static org.junit.Assert.*;
 public class MyTest {
 
     @Test
-    public void testConcatenate() {
+    public void testConcatenate1() {
         String result = MyMain.concatenate("one", "two");
 
         assertEquals("onetwo", result);
+    }
 
+    @Test
+    public void testConcatenate2() {
+        String result = MyMain.concatenate("one", "three");
+
+        assertEquals("onethree", result);
+    }
+
+    @Test
+    public void testConcatenate3() {
+        String result = MyMain.concatenate("one", "four");
+
+        assertEquals("onefour", result);
+    }
+
+    @Test
+    public void testConcatenate4() {
+        String result = MyMain.concatenate("one", "five");
+
+        assertEquals("onetwo", result);
     }
 }
