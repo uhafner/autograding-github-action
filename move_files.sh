17d9c380bbfc82169526254c142902da20b1993f
Tobias Effner <me@tobiasmichael.de>
2020-07-09 16:48:22 +0200

    Fixed build_container.yml and move_files.sh

--- move_files.sh
+++ move_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mv /resources/ /target/
-mkdir -p /target/surefire-reports/
-mv /target/TEST-* /target/surefire-reports/
-mkdir -p /target/site/jacoco/
-mv /target/jacoco.xml /target/site/jacoco/
-mkdir -p /target/pit-reports/
-mv /target/mutations-few.xml /target/pit-reports/
+mv src/test/resources/ target/
+mkdir -p target/surefire-reports/
+mv target/TEST-* target/surefire-reports/
+mkdir -p target/site/jacoco/
+mv target/jacoco.xml target/site/jacoco/
+mkdir -p target/pit-reports/
+mv target/mutations-few.xml target/pit-reports/
