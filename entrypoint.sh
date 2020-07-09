82e27208f9083e940a19d9fb1b2f0e54116f9de1
Tobias Effner <me@tobiasmichael.de>
2020-07-09 17:34:22 +0200

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,6 @@ mvn -ntp org.pitest:pitest-maven:mutationCoverage
 # Build with maven (jacoco)
 mvn -ntp -V -U -e jacoco:prepare-agent test jacoco:report -Dmaven.test.failure.ignore
 
-#if [ ! -d "/target" ]; then
-#  mv /resources/ /target/
-#  mkdir -p /target/surefire-reports/
-#  mv /target/TEST-* /target/surefire-reports/
-#  mkdir -p /target/site/jacoco/
-#  mv /target/jacoco.xml /target/site/jacoco/
-#  mkdir -p /target/pit-reports/
-#  mv /target/mutations-few.xml /target/pit-reports/
-#fi
-
 # Get report
-java -jar /jars/github-actions-autograding.jar $CONFIG $TOKEN $DEBUG
+java -jar /jars/github-actions-autograding.jar "$CONFIG" $TOKEN $DEBUG
 
