a3a7b5ab78ef4e360f1a15b3131ff10b862cf5dd
Ulli Hafner <ullrich.hafner@gmail.com>
2020-09-11 13:52:07 +0200

    Remove maven build.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# JUnit, CMD, Checkstyle, FindBugs
-mvn -ntp -V -e clean verify -Dmaven.test.failure.ignore -Dgpg.skip
-# Build with maven (pit)
-mvn -ntp org.pitest:pitest-maven:mutationCoverage
-# Build with maven (jacoco)
-mvn -ntp -V -U -e jacoco:prepare-agent test jacoco:report -Dmaven.test.failure.ignore
-
-# Get report
 java -jar /target/github-actions-autograding-jar-with-dependencies.jar
 
