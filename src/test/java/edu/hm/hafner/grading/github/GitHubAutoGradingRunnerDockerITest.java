329767cf201934e4543206f825b2aca4449d82c1
Ulli Hafner <ullrich.hafner@gmail.com>
2024-02-29 15:29:35 +0100

    Bump version to 3.15.0.

--- src/test/java/edu/hm/hafner/grading/github/GitHubAutoGradingRunnerDockerITest.java
+++ src/test/java/edu/hm/hafner/grading/github/GitHubAutoGradingRunnerDockerITest.java
@@ -218,7 +218,7 @@ public class GitHubAutoGradingRunnerDockerITest {
     }
 
     private GenericContainer<?> createContainer() {
-        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-github-action:3.14.1-SNAPSHOT"));
+        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-github-action:3.15.0"));
     }
 
     private String readStandardOut(final GenericContainer<? extends GenericContainer<?>> container) throws TimeoutException {
