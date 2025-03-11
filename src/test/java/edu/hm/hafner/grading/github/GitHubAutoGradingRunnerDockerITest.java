e2660c125489dc55f2579e4932b923f02db37516
Ulli Hafner <ullrich.hafner@gmail.com>
2025-03-11 13:37:04 +0100

    Prepare next development version 5.1.0

--- src/test/java/edu/hm/hafner/grading/github/GitHubAutoGradingRunnerDockerITest.java
+++ src/test/java/edu/hm/hafner/grading/github/GitHubAutoGradingRunnerDockerITest.java
@@ -218,7 +218,7 @@ public class GitHubAutoGradingRunnerDockerITest {
     }
 
     private GenericContainer<?> createContainer() {
-        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-github-action:5.0.0"));
+        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-github-action:5.1.0-SNAPSHOT"));
     }
 
     private String readStandardOut(final GenericContainer<? extends GenericContainer<?>> container) throws TimeoutException {
